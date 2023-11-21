fc3334226fdbcc78145c012b64c691b25a2f487c
Tayfun DURMAZ <durmaztayfun178@gmail.com>
2023-11-22 01:25:38 +0300

    MongoDB url kesildi

--- app.js
+++ app.js
@@ -1,47 +1,35 @@
-// process.chdir(__dirname);
 
-
-// const { error } = require('console');
 const express = require('express');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 const { dolarAl } = require('./mat.js');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
 
 const app = express();
+dotenv.config();
 const path = require('path');
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
-const port = 5050;
+const port = 3001;
 
 
-const dbURI = 'mongodb+srv://tayfunDurmaz:t1234@todoapp.pqvymtb.mongodb.net/todoApp?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((resulte) => {
-        console.log('connect to db');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-
-// app.get('/add-Blog', (req,res) => {
-//     const blog = new Blog ({
-//         body: 'Benim adım NECATİ DURMAZ'
-//     });
-
-//     blog.save()
-//         .then((result) => {
-//             res.send(result);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// });
-
-
-
+const connectDatabase =async ()=> {
+    try {
+      const url = process.env.MONGO;
+      await mongoose.connect(url, {
+        useNewUrlParser: true, useUnifiedTopology: true,
+      });
+      console.log('connect to database')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  connectDatabase()
 app.get('/all-Blog', (req,res) => {
     Blog.find()
         .then((result) => {
@@ -67,33 +55,19 @@ app.get('/', (req,res) => {
 
 
 app.get('/veri', (req,res) => {
-   
-    // console.log("verii sayfasındasın");
 
-   
+    try {
+        Blog.find().sort({ createdAt: -1 })
+            .then(result => {
+                res.json(result);
+            })
+            .catch(err => {
+            console.log(err);
+            });
+    } catch (error) {
+        console.log(`yarrammmm ${error}`)
+    }
 
-    Blog.find().sort({ createdAt: -1 })
-        .then(result => {
-            res.json(result);
-        })
-        .catch(err => {
-        console.log(err);
-        });
-
-    // try {
-    //     if(array.length === 0){
-    //         throw new Error("Array içi boş mal");
-    //     }
-    //     console.log(array);
-    //     res.json(array);
-    // } catch (error) {
-    //     console.log(error);
-    //     const errmsg = {
-    //         hata : error.message,
-    //     };
-    //     res.json(errmsg);
-    // }
-    
 });
 
 app.post('/' , async (req,res) => { // req ---> istek    res ---> cevap
